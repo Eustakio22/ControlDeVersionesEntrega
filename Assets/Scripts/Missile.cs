c432bd19c2046be696a681716fc8901055a62ab5
Eustakio22 <gerardros222@gmail.com>
2021-10-20 11:35:32 +0200

    Primeros cambios

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -6,7 +6,7 @@ public class Missile : MonoBehaviour
 {
     public void SetTarget(Transform target)
     {
-        var direction = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x);
-        transform.Rotate(new Vector3(0, 0, direction));
+        /*var direction = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x);
+        transform.Rotate(new Vector3(0, 0, direction));*/
     }
 }
